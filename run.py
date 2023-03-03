504938274497717905a10841c53784149bf683a9
gabrielqueeiroz <gabriel.dequeirozsousa@gmail.com>
2023-03-03 12:14:05 -0400

    connection

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_world():
     return 'Olá estou online'
+
+@app.route("/insert", methods=["POST"])
+def insert():
+    userRepo = UserRepo()
+    body = request.json
+
+    userRepo.insert_user(body["name"])
+
+    return 'OK'
